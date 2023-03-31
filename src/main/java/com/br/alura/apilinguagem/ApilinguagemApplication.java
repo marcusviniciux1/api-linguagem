538e0388ac475fe3f4dfea48dc1e592511474c36
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2023-03-30 23:54:38 -0300

    criando API com Spring

---
+++ src/main/java/com/br/alura/apilinguagem/ApilinguagemApplication.java
@@ -0,0 +1,13 @@
+package com.br.alura.apilinguagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilinguagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilinguagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/alura/apilinguagem/ApilinguagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alura.apilinguagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilinguagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
